12a30a17ce3b14ba831f492aecd19ca01a35abfb
nggill <92691516+nggill@users.noreply.github.com>
2022-04-28 08:54:02 -0500

    added two questions to js

---
+++ script.js
@@ -0,0 +1,19 @@
+const quizData = [
+    {
+        question: 'How old is Mickey Mouse?',
+        a: '30',
+        b: '55',
+        c: '92',
+        d: '102',
+        correct: 'c'
+    },{
+        question: 'What is the most used programming language in 2022?',
+        a: 'Python',
+        b: 'C#',
+        c: 'Java',
+        d: 'JavaScript',
+        correct: 'a',
+    },{
+
+    }
+]
